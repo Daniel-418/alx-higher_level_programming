23cf5ada0df4323a59eb4310510b91a1fd0e01bb
Daniel <komolafedanielolaoluwa@gmail.com>
2023-03-08 17:31:44 -0800

    fixed some more issues

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,6 +7,6 @@ def fizzbuzz():
         elif i % 3 == 0:
             print("{} ".format("Fizz"), end="")
         elif i % 5 == 0:
-            print("{} ".format("Buzz"), end="") 
+            print("{} ".format("Buzz"), end="")
         else:
             print("{} ".format(i), end="")
