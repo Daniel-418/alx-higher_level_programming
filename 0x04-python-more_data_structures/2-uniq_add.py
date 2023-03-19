e0ea90da8ec3ac0fe96c6055b73661cb4a23eff6
Daniel <komolafedanielolaoluwa@gmail.com>
2023-03-18 17:27:16 -0700

    fixed semicolon problem with __2. Unique addition__

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    count = 0;
-    uniq_list = set(my_list);
+    count = 0
+    uniq_list = set(my_list)
 
     for i in uniq_list:
         count += i
