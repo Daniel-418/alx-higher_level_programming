871f2e197e0ddd97ff435515df324970ec2ca35f
Daniel <komolafedanielolaoluwa@gmail.com>
2023-03-28 18:28:22 -0700

    fixed an issue with the first task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
 
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end=" ")
+            print("{}".format(my_list[i]), end="")
             num_of_elements += 1
         except(IndexError):
             print()
