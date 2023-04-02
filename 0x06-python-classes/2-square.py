4817d218b55d9262a0d2d676fc6e8db94c0724c7
Daniel <komolafedanielolaoluwa@gmail.com>
2023-04-01 22:25:44 -0700

    Finished task __2. size validation__

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Creates a square with size attribute"""
+
+
+class Square:
+    """A class representing a square"""
+
+    def __init__(self, size=0):
+        """Constructor to initialize the square
+
+        Args:
+            size (int): The size of the square.
+
+        Returns:
+            A square object
+        """
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
