873772047f639f310438c137dd88e93b0c34043f
Daniel <komolafedanielolaoluwa@gmail.com>
2023-04-03 16:40:26 -0700

    Finished task 2 and 3

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""This module defines a Rectangle class"""
+
+
+class Rectangle:
+    """This class defines an empty Rectangle object"""
+
+    def __init__(self, width=0, height=0):
+        """Constructor for the Rectangle object
+
+        Args:
+            width (int): The width of the Rectangle
+            height (int): The height of the Rectangle
+
+        Return: A Rectangle object
+        """
+
+        if (Rectangle.__validate_parameter(width, "width") and
+                Rectangle.__validate_parameter(height, "height")):
+            self.__width = width
+            self.__height = height
+
+    @staticmethod
+    def __validate_parameter(parameter, string):
+        """Utility method to validate the values passed
+            as the object's attributes
+
+        Args:
+            parameter (int): The number to be validated
+            string (str): The string representation of the parameter to pass
+                            to the error message
+
+            Return: True if the parameter is valid. False if otherwise
+        """
+
+        if type(parameter) is not int:
+            raise TypeError(string + " must be an integer")
+            return False
+        elif parameter < 0:
+            raise ValueError(string + " must be >= 0")
+            return False
+        else:
+            return True
+
+    @property
+    def width(self):
+        """Width property to set and get the width value of this Rectangtle"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if Rectangle.__validate_parameter(value, "width"):
+            self.__width = value
+
+    @property
+    def height(self):
+        """Height property to set and get the height value of this Rectangle"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if Rectangle.__validate_parameter(value, "height"):
+            self.__height = value
+
+    def area(self):
+        """Calculates the area of this Rectangle
+
+        Returns: The area of the rectangle
+        """
+
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Calculates the perimeter of this Rectangle
+
+        Returns: The perimeter of this rectangle
+        """
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+
