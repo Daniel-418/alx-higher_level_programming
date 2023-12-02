c66113e86d6b3819e79475ce31ec8ed53d27c0b6
Daniel <komolafedanielolaoluwa@gmail.com>
2023-12-01 17:07:19 -0800

    .

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for i in range(26):
     letter = chr(97 + i)
     if (letter == 'q' or letter == 'e'):
         continue
-    print(f"{}".format(letter), end="")
+    print("{}".format(letter), end="")
