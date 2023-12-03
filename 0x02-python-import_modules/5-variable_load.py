a05c2ff51abbc7e75241374a131b64ba92e3f7da
Daniel <komolafedanielolaoluwa@gmail.com>
2023-12-02 22:44:18 -0800

    finished all the mandatory task

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module that prints a value from another module"""
+
+from variable_load_5 import a
+
+
+def main():
+    print("{}".format(a))
+
+
+if __name__ == "__main__":
+    main()
