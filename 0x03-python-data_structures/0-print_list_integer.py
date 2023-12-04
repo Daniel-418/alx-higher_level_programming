0b3d936b7a8d2e831377aaf37aefa640b756b5ca
Daniel <komolafedanielolaoluwa@gmail.com>
2023-12-03 18:12:50 -0800

    finished the first task

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    if (len(my_list) > 0):
-        for item in my_list:
-            print("{}".format(item))
+    for i in my_list:
+        print("{:d}".format(i))
