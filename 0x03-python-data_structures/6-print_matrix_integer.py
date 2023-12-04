e8ad0568abcb0138cf216b247652c754957a53d7
Daniel <komolafedanielolaoluwa@gmail.com>
2023-12-03 19:13:07 -0800

    did the seventh task

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, column in enumerate(row):
+            if i == len(row) -1:
+                print("{:d}".format(column))
+            else:
+                print("{:d}".format(column), end = " ")
