39ebf22870c311ebcbfff491e49a540c94a5feeb
Daniel <komolafedanielolaoluwa@gmail.com>
2023-12-10 14:32:40 -0800

    fixed number 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -11,6 +11,8 @@ def max_integer(my_list=[]):
     if (len(my_list) == 0):
         return None
 
+    maximum = my_list[0]
+
     for item in my_list:
         if (item > maximum):
             maximum = item
