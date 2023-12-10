8437477a097643a6da4e9d9a4d3dba357d26a4e6
Daniel <komolafedanielolaoluwa@gmail.com>
2023-12-10 14:23:57 -0800

    finished the 8th task

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    """
+    Finds the length and first character of
+    a sentence
+    :pamam sentence: The sentence
+    :Return: Tuple containing the first character
+    and length of the sentence
+    """
+    sentence_len = len(sentence)
+    if (sentence_len == 0):
+        return None
+
+    return (sentence_len, sentence[0])
