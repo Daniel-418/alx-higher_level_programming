3999c85d3ab9903293ce7f85d63770cfe86c84e1
Daniel <komolafedanielolaoluwa@gmail.com>
2023-12-10 14:25:13 -0800

    fixed an issue with the 8th task

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -10,6 +10,6 @@ def multiple_returns(sentence):
     """
     sentence_len = len(sentence)
     if (sentence_len == 0):
-        return None
+        return (sentence_len, None)
 
     return (sentence_len, sentence[0])
