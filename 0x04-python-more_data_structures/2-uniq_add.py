edabc697ab0aa42ff8a43266c589c605d18a8b7c
Daniel <komolafedanielolaoluwa@gmail.com>
2023-12-10 17:48:13 -0800

    safd

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    total = 0
-    uniq_list = set(my_list)
-
-    for item in uniq_list:
-        total += item
-
-    return total
+    return (sum(set(my_list)))
