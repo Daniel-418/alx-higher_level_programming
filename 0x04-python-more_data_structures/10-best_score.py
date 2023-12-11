eab49202f076a9e2d6eb2caebe9ef2c9e72d3045
Daniel <komolafedanielolaoluwa@gmail.com>
2023-12-10 20:24:14 -0800

    did task number 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    maximum = list(a_dictionary.keys())[0]
+    for key in a_dictionary.keys():
+        if a_dictionary[key] > a_dictionary[maximum]:
+            maximum = key
+
+    return maximum
