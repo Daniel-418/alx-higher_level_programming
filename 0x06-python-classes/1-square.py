3b8baf6b845052132367d6d1fb8e090568f8c775
Daniel <komolafedanielolaoluwa@gmail.com>
2023-12-19 18:17:41 -0800

    finished the second task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a square class"""
+
+
+class Square:
+    """
+    A square class with one private attribute
+    """
+
+    def __init__(self, size):
+        """
+        initializes the class
+        @param size: The size of the square
+        """
+        self.__size = size
