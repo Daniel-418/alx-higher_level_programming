bb92d89f4d6e1d20e816fd5458e383fe07a9b9cc
Daniel <komolafedanielolaoluwa@gmail.com>
2023-12-19 18:14:06 -0800

    did the first task

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a square class"""
+
+
+class Square:
+    """
+    An empty square class
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+README for the classes project on alx
