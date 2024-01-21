df07505c4113fde40873b2613415ec79ca27b0b8
Komolafe Daniel <komolafedanielolaoluwa@gmail.com>
2024-01-20 20:20:45 -0800

    added files for the base class

---
+++ projects/0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyton3
+"""Defines a Base Class"""
+
+
+class Base:
+    """A base model class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
