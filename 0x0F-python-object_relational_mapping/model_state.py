081af4cda4e10e94b9c80a313be5349118ec0f40
Daniel <komolafedanielolaoluwa@gmail.com>
2024-03-21 19:16:42 -0700

    added the file for the sixth file

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Uses sql alchemy to create a table in a database """
+
+
+from SQLAlchemy import Column, Integer, String
+from SQLAlchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ States class
+    Attributes:
+        __tablename__ (str): The name of the table
+        id (int): The state id of the class
+        name (string): The state name of the class
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
