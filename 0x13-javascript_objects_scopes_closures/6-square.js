7dbf6d4c263ced5ba7e8a13d939e8ff916f7dfb4
Daniel <komolafedanielolaoluwa@gmail.com>
2024-03-31 14:55:02 -0700

    added all the files

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let str = '';
+        for (let j = 0; j < this.width; j++) {
+          str += c;
+        }
+        console.log(str);
+      }
+    }
+  }
+}
+
+module.exports = Square;
