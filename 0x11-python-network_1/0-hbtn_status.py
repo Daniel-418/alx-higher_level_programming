145b8311cb8436592e78d44c8368eb660deb9e36
Daniel <komolafedanielolaoluwa@gmail.com>
2024-03-31 19:11:21 -0700

    added all the files

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Script that fetches the content of a url """
+import urllib.request
+
+
+def main():
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resource:
+        content = resource.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
+if __name__ == "__main__":
+    main()
