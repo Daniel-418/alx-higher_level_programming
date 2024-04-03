5c7e78478b80ba637be76c580d7b99fd1bba1497
Daniel <komolafedanielolaoluwa@gmail.com>
2024-04-02 21:33:05 -0700

    .

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Script that fetches the content of a url """
+import requests
+import sys
+
+
+def main():
+    response = requests.get(sys.argv[1])
+    print("{}".format(response.headers['X-Request-Id']))
+
+
+if __name__ == "__main__":
+    main()
