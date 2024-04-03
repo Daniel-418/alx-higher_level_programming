dbbb3ddad69c6059fb846042368dd0de64d14503
Daniel <komolafedanielolaoluwa@gmail.com>
2024-04-02 21:24:56 -0700

    .

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 def main():
     response = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t - type: {}".format(type(response.text))))
+    print("\t - type: {}".format(type(response.text)))
     print("\t - content: {}".format(response.text))
 
 
